aa2e1dfab609c965c045bf7ff40d77d44251d16d
Techrish Team <techrishtasks@gmail.com>
2023-09-12 09:57:06 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# cicd-pipeline
+This is to learn git cicd to aws ec2
---
+++ server.js
@@ -0,0 +1,15 @@
+const express   =  require("express");
+
+const app       =  express();
+
+
+app.get("/",(req,res)=>{
+    res.json({
+        status  : 200,
+        message : "Hello World!"
+    })
+});
+
+app.listen(8080, ()=>{
+    console.log("Server run on port 8080")
+})
